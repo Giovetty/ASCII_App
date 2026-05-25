f74a236445f802b4608eded4ca80fab566bbec23
Giovetty <kelsia20giovetty@gmail.com>
2026-05-25 14:07:38 +0200

    Added variable to text view

--- app/src/main/java/com/example/ascii_app/MainActivity.kt
+++ app/src/main/java/com/example/ascii_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ascii_app
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.artTxt)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
